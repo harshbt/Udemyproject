32f2d8eac696857f336df6f026c11f9782b1f20a
harsh <Braintech@SYS46>
2022-02-11 17:37:37 +0530

    Udemy Project Started

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,26 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is a simple test', 'https://static.onecms.io/wp-content/uploads/sites/9/2021/02/12/roast-chicken-with-chile-basil-vinaigrette-charred-broccoli-potatoes-FT-RECIPE0321.jpg'),
+    new Recipe('Another Test Recipe', 'This is a simple test', 'https://static.onecms.io/wp-content/uploads/sites/9/2021/02/12/roast-chicken-with-chile-basil-vinaigrette-charred-broccoli-potatoes-FT-RECIPE0321.jpg')
+  ];
+
+  @Output() recipeSelected = new EventEmitter<Recipe>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onSelected(recipe: Recipe){
+    this.recipeSelected.emit(recipe);
+  }
+
+}
